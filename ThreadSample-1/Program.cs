069d667c4ef272a9f28c494a09d087e2caa70f7b
Fatih Koçak <fatihkocak@hotmail.com>
2019-11-21 17:35:54 +0300

    genesis

---
+++ ThreadSample-1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadSample_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ///program çlışınca single thread(main thread) oluşur.
+            ///yeni bir thread başlatmak için thread class'ından yeni bir örnek almak gerekir. Bunuda Thread.Start metotu sağlar.
+            
+
+
+        }
+
+        private static void DoSomething()
+        {
+            Console.WriteLine("Caling DoSomething() method...");
+        }
+    }
+}
