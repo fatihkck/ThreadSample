e3997799355723b0ea45d6ed032115c8a4de2a52
Fıtı Fıtı <fatihkocak@hotmail.com>
2019-12-01 23:15:54 +0300

    .

---
+++ 19-ReturnDataWithThread/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _19_ReturnDataWithThread
+{
+    public delegate void SumOfNumbersCallback(int sumOfNumber);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Thread ile Parametre ve değer dönderme örneği
+
+            Console.WriteLine("Lütfen numara giriniz");
+            int target = Convert.ToInt32(Console.ReadLine());
+
+
+            SumOfNumbersCallback callBack = new SumOfNumbersCallback(PrintSumOfNumber);
+
+            CalculateNumber calculateNum = new CalculateNumber(target, callBack);
+
+            Thread t1 = new Thread(calculateNum.PrintSumOfNumbers);
+            t1.Start();
+
+            Console.WriteLine("Main thread end");
+            Console.Read();
+        }
+
+        public static void PrintSumOfNumber(int sum)
+        {
+            Console.WriteLine("Sum of number = {0}", sum);
+        }
+    }
+
+    public class CalculateNumber
+    {
+
+        int _target;
+        SumOfNumbersCallback _callBackMethod;
+
+        public CalculateNumber(int target,SumOfNumbersCallback callBackMethod)
+        {
+            _target = target;
+            _callBackMethod = callBackMethod;
+        }
+
+        public void PrintSumOfNumbers()
+        {
+            int sum = 0;
+            for (int i = 1; i <= _target; i++)
+            {
+                Console.WriteLine(i);
+                sum += i;
+            }
+
+            _callBackMethod?.Invoke(sum);
+
+        }
+    }
+}
--- ThreadSample-3/Program.cs
+++ ThreadSample-3/Program.cs
@@ -13,27 +13,68 @@ namespace ThreadSample_3
         {
 
 
-            Thread t = new Thread(DoSomeThing);
-            Thread t1 = new Thread(DoSomeThing);
-
-
-            t.Start();
-            t.Join();
-
+            Thread t1 = new Thread(Thread1Funciton);
             t1.Start();
-            
 
-            Console.WriteLine("Press Enter to terminate!");
+            Thread t2 = new Thread(Thread2Funciton);
+            t2.Start();
+
+            //Belirtilen sürede bitmezse t1 therad beklemeden devam eder.
+            //t1.Join(1000);
+
+            if (t1.Join(1000))
+            {
+                Console.WriteLine("Thread1Funciton completed");
+            }
+            else
+            {
+                Console.WriteLine("Thread1Funciton has not completed 1 second");
+
+            }
+
+            //Console.WriteLine("Thread1Funciton completed");
+
+            t2.Join();
+            Console.WriteLine("Thread2Funciton completed");
+
+
+            //if (t1.IsAlive)
+            //{
+            //    Console.WriteLine("Thread1Funciton is still doing it's work");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Thread1Funciton completed");
+            //}
+
+            while (t1.IsAlive)
+            {
+                Console.WriteLine("Thread1Funciton is still doing it's work");
+                Thread.Sleep(500);
+            }
+
+            if (!t1.IsAlive)
+            {
+                Console.WriteLine("Thread1Funciton completed");
+            }
+
+            Console.WriteLine("Main Complate");
             Console.ReadLine();
 
         }
 
 
-        public static void DoSomeThing()
+        public static void Thread1Funciton()
         {
-            Console.WriteLine("Test");
-
-            Thread.Sleep(2000);
+            Console.WriteLine("Thread1Funciton started");
+            Thread.Sleep(3000);
+            Console.WriteLine("Thread1Funciton is about to return");
         }
+
+        public static void Thread2Funciton()
+        {
+            Console.WriteLine("Thread2Funciton started");
+        }
+
     }
 }
