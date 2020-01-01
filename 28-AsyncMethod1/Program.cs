95de2b892b8c1273d41ad67cfb41e4d569d57c6f
Fıtı Fıtı <fatihkocak@hotmail.com>
2020-01-01 22:47:59 +0300

    .

---
+++ 28-AsyncMethod1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace _28_AsyncMethod1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                // Start computation.
+                Example();
+                // Handle user input.
+                string result = Console.ReadLine();
+                Console.WriteLine("You typed: " + result);
+            }
+        }
+
+
+        static async void Example()
+        {
+            // This method runs asynchronously.
+            int t = await Task.Run(() => Allocate());
+            Console.WriteLine("Compute: " + t);
+        }
+
+        static int Allocate()
+        {
+
+            // Compute total count of digits in strings.
+            int size = 0;
+            for (int z = 0; z < 100; z++)
+            {
+                for (int i = 0; i < 1000000; i++)
+                {
+                    string value = i.ToString();
+                    size += value.Length;
+                }
+            }
+            return size;
+        }
+    }
+}
---
+++ 29-AsyncMethod2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _29_AsyncMethod2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            // Call async method 10 times.
+            for (int i = 0; i < 30; i++)
+            {
+                Run2Methods(i);
+            }
+            // The calls are all asynchronous, so they can end at any time.
+
+            Console.WriteLine("done");
+            Console.ReadLine();
+        }
+
+        static async void Run2Methods(int count)
+        {
+            // Run a Task that calls a method, then calls another method with ContinueWith.
+            int result = await Task.Run(() => GetSum(count))
+                .ContinueWith(task => MultiplyNegative1(task));
+            
+            Console.WriteLine("Run2Methods result: " + result);
+        }
+
+        static int GetSum(int count)
+        {
+            Thread.CurrentThread.Name = "name" + count;
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            //Thread.Sleep(1000);
+            // This method is called first, and returns an int.
+            int sum = 0;
+            for (int z = 0; z < count; z++)
+            {
+                sum += (int)Math.Pow(z, 2);
+            }
+            return sum;
+        }
+
+        static int MultiplyNegative1(Task<int> task)
+        {
+            // This method is called second, and returns a negative int.
+            return task.Result * -1;
+        }
+
+
+        async static void BackgroundMethod()
+        {
+            // Use a local function.
+            void InnerMethod()
+            {
+                while (true)
+                {
+                    System.Threading.Thread.Sleep(150);
+                    Console.WriteLine("::Background::");
+                }
+            }
+            // Create a new Task and start it.
+            // ... Call the local function.
+            var task = new Task(() => InnerMethod());
+            task.Start();
+            await task;
+        }
+    }
+}
